e770a1d85852a7f5db3a1d5a39e168f19d5222ea
Nishant Jawla <40868641+NishantJawla@users.noreply.github.com>
2021-05-01 12:16:51 +0530

    Added Validations to the signup route

--- controllers/main/auth.js
+++ controllers/main/auth.js
@@ -1,7 +1,16 @@
 const User = require('../../models/user');
 const bcrypt = require('bcrypt')
 const saltRounds = 10;
+const { check, validationResult } = require("express-validator");
 exports.signupHandler = (req,res)=>{
+    const errors = validationResult(req);
+
+if (!errors.isEmpty()) {
+    return res.status(402).json({
+        location: '/controllers/main/auth.js',
+        error: errors.array()[0].msg
+    });
+}
     bcrypt.hash(req.body.plainPassword, saltRounds, (err, hash) => {
         const user = new User(req.body);
         user.encryptedPassword = hash
@@ -9,7 +18,7 @@ exports.signupHandler = (req,res)=>{
         if(err){
         return res.json({
                 location: '/controllers/main/auth.js',
-                msg: err.message,
+                msg: 'email already in use',
                 err
             })
         }
