e6c563619af9c6e0753a24bd50eb9264a2fbc0b4
Nishant Jawla <40868641+NishantJawla@users.noreply.github.com>
2021-05-05 16:41:00 +0530

    Added validation in phone number during signup

--- routes/main/auth.js
+++ routes/main/auth.js
@@ -23,6 +23,7 @@ router.post('/signup',[
     check("name", "name should be at least 3 char").isLength({ min: 4 }),
     check("email", "Please provide a valid email").isEmail(),
     check("phoneNumber", "Phone number should be 10 char long").isLength({ min: 10,max:10 }),
+    check('phoneNumber','phone number should be numeric').isNumeric(),
     check("plainPassword", "password should be at least 5 character long").isLength({ min: 5 })
     ],signupHandler);
 
