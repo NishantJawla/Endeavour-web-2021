97b17ad767a3684057a689e0e488455ef7736337
Nishant Jawla <40868641+NishantJawla@users.noreply.github.com>
2021-05-07 18:14:39 +0530

    started header

--- client/src/core/Home.js
+++ client/src/core/Home.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "../styles.css";
 import {API} from "../backend"
+import Navbar from "./components/navbar";
+
 export default function Home() {
   console.log(`API is available at${API}`,process.env.REACT_APP_BACKEND)
   return (
     <div>
+      <Navbar/>
       <h1 className="text-white">Hello front end</h1>
     </div>
   );
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import Routes from "./Routes";
 import ReactDOM from "react-dom";
-
+// import 'bootstrap/dist/css/bootstrap.min.css';
 ReactDOM.render(<Routes />, document.getElementById("root"));
