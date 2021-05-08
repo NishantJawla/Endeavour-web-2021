b1a82f36fac028d18d272e22229d3773f1a59cf9
Nishant Jawla <40868641+NishantJawla@users.noreply.github.com>
2021-05-08 12:26:31 +0530

    Added footer

--- client/src/core/Home.js
+++ client/src/core/Home.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "../styles.css";
 import { API } from "../backend";
-
+import Footer from "./components/footer/footer"
 export default function Home() {
   console.log("API IS", API);
 
   return (
     <div>
       <h1 className="text-white">Hello front end </h1>
+      <Footer/>
     </div>
   );
 }
---
+++ client/src/core/components/footer/footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./footer.css"
+function Footer() {
+    return (
+        <div>
+            <footer className="footer bg-dark mt-auto ">
+                <div className="container-fluid footer-child text-white text-center py-1">
+                <h4>Copyright © e-Cell Technical Team 2021</h4>
+                </div>
+                
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
