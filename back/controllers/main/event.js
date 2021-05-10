9429b53379869f2739875032184ea8a0e9acb98f
Nishant Jawla <40868641+NishantJawla@users.noreply.github.com>
2021-05-10 18:37:35 +0530

    updated error messages

--- back/controllers/main/event.js
+++ back/controllers/main/event.js
@@ -66,13 +66,17 @@ exports.getEventHandler = (req,res) => {
             }
             return res.status(400).json({
                 status: 400,
-                msg: "Server Error",
-                error: "Server Error"
+                msg: "Event Not Found",
+                error: "Event Not Found"
             })
         }
         let content = event
         content.paid = undefined;
-        return res.status(200).json(content)
+        return res.status(200).json({
+            status: 200,
+            msg: "Event Found Successfully"
+
+        })
     })
 }
 
