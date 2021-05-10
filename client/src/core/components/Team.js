3de8b015d120283e9abecbed60b6d3a15a70540e
pranav-develop <pranavschiller@gmail.com>
2021-05-10 21:35:38 +0530

    Team section
    created team section

--- client/src/core/components/Team.js
+++ client/src/core/components/Team.js
@@ -4,11 +4,16 @@ import EachMember from "./sub-components/EachMember";
 
 function Team(){
     return(
-        <div className="team py-5 bg-main-pattern bg-norepeat" id="events">
+        <div className="team py-5 bg-sec-pattern bg-norepeat" id="events">
             <div className="container py-5 mt-5">
                 <div className="heading-font text-center color-white fw-bold">Our Team</div>
                 <div className="events-container">
                     <div className="d-flex justify-content-between flex-wrap">
+                        <EachMember />
+                        <EachMember />
+                        <EachMember />
+                        <EachMember />
+                        <EachMember />
                         <EachMember />
                         <EachMember />
                         <EachMember />
--- client/src/core/components/sub-components/EachMember.js
+++ client/src/core/components/sub-components/EachMember.js
@@ -4,11 +4,20 @@ import teamImg from "./../../../assets/img/team-img/nishant.jpeg";
 
 function EachMember(){
     return(
-        <div className="py-5 each-member">
-            <div className="">
-                <div className="member-img py-3">
-                    <img src={teamImg} width="100%" height="100%" alt="hackathon icon"/>
-                </div>
+        <div className="m-4 py-3 each-member">
+            <div className="position-relative overflow-hidden rounded">
+                <a href="https://www.google.com">
+                    <div className="member-img">
+                        <div className="overlay bg-primary w-100 h-100 position-absolute"></div>
+                        <div href="google.com">
+                            <img src={teamImg} width="100%" height="100%" alt="hackathon icon"/>
+                        </div>
+                    </div>
+                    <div className="member-desc position-absolute bottom-0 color-white w-100 py-2">
+                        <div className="member-name ls-1 fw-bold py-1 fs-5 text-center">Nishant Jawla</div>
+                        <div className="member-position ls-1 fs-6 text-center">Technical Team</div>
+                    </div>
+                </a>
             </div>
         </div>
     );
