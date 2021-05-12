ac3a07a1451ec1b13c873de25d3c714a5803c340
Nishant Jawla <40868641+NishantJawla@users.noreply.github.com>
2021-05-12 11:12:58 +0530

    connected team with real time firebase

--- client/src/core/Home.js
+++ client/src/core/Home.js
@@ -1,5 +1,5 @@
 //jshint esversion: 8
-import React, {useEffect} from 'react';
+import React, {useEffect,useState} from 'react';
 import "../styles.css";
 import { API } from "../backend";
 import Header from "./components/Header";
@@ -8,14 +8,8 @@ import Speakers from "./components/Speakers";
 import Events from "./components/Events";
 import ContactUs from "./components/ContactUs";
 import firebase from "../firebase";
-function Home() {
-  useEffect(() => {
-    const todoRef = firebase.database().ref('team');
-    todoRef.on('value', (snapshot) => {
-      const todos = snapshot.val();
-      console.log({"content":todos});
-    });
-  }, []);
+const Home = () => {
+  
     return (
         <React.Fragment>
             <Header />
