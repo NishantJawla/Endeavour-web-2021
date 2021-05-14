4dbbe8e8df2fd70aa940e5525505f6dd92d7523e
Nishant Jawla <40868641+NishantJawla@users.noreply.github.com>
2021-05-14 19:50:14 +0530

    applied conditional rendering on read more button

--- client/src/core/components/sub-components/EachEvent.js
+++ client/src/core/components/sub-components/EachEvent.js
@@ -18,8 +18,11 @@ const  EachEvent = (props) => {
                 </div>
                 <div className="event-title fw-bolder ls-1 fs-5 py-2">{props.data.eventName}</div>
                 <div className="event-desc py-2">{props.data.eventTagline}</div>
-               
-                <Link to={url} className="learn-more border-0 bg-transparent p-0 fw-bold ls-1 mt-2 color-primary" data={props}>Read More</Link>
+                {
+                    (props.data.isOpen === "true") && (<Link to={url} className="learn-more border-0 bg-transparent p-0 fw-bold ls-1 mt-2 color-primary" 
+                    data={props}>Read More</Link>)
+                }
+                
             </div>
         </div>
     );
