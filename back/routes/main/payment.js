2eced9e6c927da23bb9e9b47fbb29545198e9481
Nishant Jawla <40868641+NishantJawla@users.noreply.github.com>
2021-05-16 19:04:04 +0530

    Created payment routes

--- back/routes/main/payment.js
+++ back/routes/main/payment.js
@@ -6,45 +6,74 @@ const Team = require('../../models/team');
 const User = require('../../models/user');
 const router = express.Router();
 const shortid = require('shortid')
-router.post("/orders/:eventId", async (req, res) => {
+router.post("/orders/eventpass", async (req, res) => {
+
     try {
-        const event = await Event.findOne({_id: req.params.eventId})
-        try {
-            const instance = new Razorpay({
-                key_id: process.env.RAZORPAY_KEY_ID,
-                key_secret: process.env.RAZORPAY_SECRET,
-            });
-            
-        const payment_capture = 1
-        const amount = parseInt(event.price)
-        const currency = 'INR'
-    
-        const options = {
-            amount: amount * 100,
-            currency,
-            receipt: shortid.generate(),
-            payment_capture
-        }
-        try {
-            const response = await instance.orders.create(options)
-            // console.log(response)
-            res.json({
-                id: response.id,
-                currency: response.currency,
-                amount: response.amount
-            })
-        } catch (error) {
-            console.log(error)
-        }
+        const instance = new Razorpay({
+            key_id: process.env.RAZORPAY_KEY_ID,
+            key_secret: process.env.RAZORPAY_SECRET,
+        });
+        
+    const payment_capture = 1
+    const amount = parseInt(150)
+    const currency = 'INR'
 
-    } catch (error) {
-        res.status(500).send(error);
+    const options = {
+        amount: amount * 100,
+        currency,
+        receipt: shortid.generate(),
+        payment_capture
     }
-    
-} catch (error) {
-    console.log(error)
-}
+    try {
+        const response = await instance.orders.create(options)
+        // console.log(response)
+        res.json({
+            id: response.id,
+            currency: response.currency,
+            amount: response.amount
+        })
+    } catch (error) {
+        console.log(error)
+    }
 
+} catch (error) {
+    res.status(500).send(error);
+}
+});
+
+router.post("/orders/intership", async (req, res) => {
+
+    try {
+        const instance = new Razorpay({
+            key_id: process.env.RAZORPAY_KEY_ID,
+            key_secret: process.env.RAZORPAY_SECRET,
+        });
+        
+    const payment_capture = 1
+    const amount = parseInt(50)
+    const currency = 'INR'
+
+    const options = {
+        amount: amount * 100,
+        currency,
+        receipt: shortid.generate(),
+        payment_capture
+    }
+    try {
+        const response = await instance.orders.create(options)
+        // console.log(response)
+        res.json({
+            id: response.id,
+            currency: response.currency,
+            amount: response.amount
+        })
+    } catch (error) {
+        console.log(error)
+    }
+
+} catch (error) {
+    res.status(500).send(error);
+}
 });
 
 router.post('/verification', (req, res) => {
