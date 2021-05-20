a308715877463360747744d547ee499ae6a2df51
Nishant Jawla <40868641+NishantJawla@users.noreply.github.com>
2021-05-20 17:16:34 +0530

    Updates causing toubles

--- back/routes/main/user.js
+++ back/routes/main/user.js
@@ -67,14 +67,10 @@ router.post("/changePassword",passport.authenticate('jwt',{session: false,failur
 router.post("/updateProfile",[
     check("branch")
     .notEmpty()
-    .withMessage("Branch Field is Required")
-    .matches(/^[a-zA-Z_ ]*$/, "i")
-    .withMessage("Branch Field is inValid"),
+    .withMessage("Branch Field is Required"),
     check("college")
     .notEmpty()
-    .withMessage("College Name is Required")
-    .matches(/^[a-zA-Z_ ]*$/, "i")
-    .withMessage("College Name Field is inValid"),
+    .withMessage("College Name is Required"),
     check("semester")
     .notEmpty()
     .withMessage("Semester is Required")
