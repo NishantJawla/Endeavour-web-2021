24f37660e461e009b1a5cf283394492ef5816c67
pranav-develop <pranavschiller@gmail.com>
2021-05-22 14:32:06 +0530

    Changes
    Added hackathon link,
    added speakers and made speaker popup

--- client/src/core/components/SpeakersPage.js
+++ client/src/core/components/SpeakersPage.js
@@ -1,35 +1,42 @@
 //jshint esversion: 8
 import React, {useEffect,useState} from "react";
 import EachSpeakerCard from "./sub-components/EachSpeakerCard";
-import firebase from "../../firebase"
+import EachSpeakerPopup from "./sub-components/EachSpeakerPopup";
+import firebase from "../../firebase";
 const SpeakersPage = () => {
     
     const [speakerData, setspeakerData] = useState();
-  useEffect(() => {
+    const [popupData, setPopupData] = useState();
+    const [showSlowly, setShowSlowly] = useState(false);
 
-    const speakerRef = firebase.database().ref('speakers');
-    speakerRef.on('value', (snapshot) => {
-      const speakers = snapshot.val();
-      const speakerData = [];
-      for (let id in speakers) {
-        speakerData.push({ id, ...speakers[id] });
-      }
-      setspeakerData(speakerData);
-    });
-  }, []);
-  useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
+    useEffect(() => {
+
+      const speakerRef = firebase.database().ref('speakers');
+      speakerRef.on('value', (snapshot) => {
+        const speakers = snapshot.val();
+        const speakerData = [];
+        for (let id in speakers) {
+          speakerData.push({ id, ...speakers[id] });
+        }
+        setspeakerData(speakerData);
+      });
+    }, []);
+    useEffect(() => {
+      window.scrollTo(0, 0)
+    }, []);
     return (
         <div className="py-5 position-relative speakers bg-sec-pattern">
             <div className="container py-5">
             <div className="heading-font pt-5 pb-3 text-center color-white fw-bold">Our Guests</div>
                 <div className="speaker-outer-box d-flex justify-content-between flex-wrap">
-                    {speakerData
-    ? speakerData.map((todo, index) => <EachSpeakerCard data={todo} key={index} />)
-    : ''}
+                    {
+                        speakerData
+                        ? speakerData.map((todo, index) => <EachSpeakerCard data={todo} setPopupData={setPopupData} setShowSlowly={setShowSlowly} key={index} />)
+                        : ''
+                    }
                 </div>
             </div>
+            <EachSpeakerPopup showSlowly={showSlowly} setShowSlowly={setShowSlowly} data={popupData} />
         </div>
     );
 }
